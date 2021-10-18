ed98740f834764427740c26f2db91db0e4d87149
Elziekio <anthony.deamer@hotmail.com>
2021-10-18 12:32:15 +1000

    Adding function for activating the service worker

--- public/js/service-worker.js
+++ public/js/service-worker.js
@@ -14,13 +14,32 @@ const CACHE_NAME = "static-cache";
 const DATA_CACHE_NAME = "data-cache";
 
 // install Service worker
-self.addEventListener("install", function(evt) {
+self.addEventListener("install", function (evt) {
     evt.waitUntil(
-      caches.open(CACHE_NAME).then(cache => {
-        console.log("Your files were pre-cached successfully!");
-        return cache.addAll(FILES_TO_CACHE);
-      })
+        caches.open(CACHE_NAME).then(cache => {
+            console.log("Your files were pre-cached successfully!");
+            return cache.addAll(FILES_TO_CACHE);
+        })
     );
-  
+
     self.skipWaiting();
-  });
+});
+
+// Activating Service worker.
+self.addEventListener("activate", function (evt) {
+    evt.waitUntil(
+        caches.keys().then(keyList => {
+            return Promise.all(
+                keyList.map(key => {
+                    if (key !== CACHE_NAME && key !== DATA_CACHE_NAME) {
+                        console.log("Removing old cache data", key);
+                        return caches.delete(key);
+                    }
+                })
+            );
+        })
+    );
+
+    self.clients.claim();
+});
+
