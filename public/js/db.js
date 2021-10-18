1a1361cd7d38a024092e8d7822e4402490ca638d
Elziekio <anthony.deamer@hotmail.com>
2021-10-18 21:22:08 +1000

    Adding uploadTransaction function

--- public/js/db.js
+++ public/js/db.js
@@ -28,3 +28,42 @@ function saveRecord(record) {
 
     budgetObjectStore.add(record);
 };
+
+// function that will handle collecting all of the data 
+function uploadTransaction() {
+    const transaction = db.transaction(['new_transaction'], 'readwrite');
+
+    const budgetObjectStore = transaction.objectStore('new_transaction');
+
+    const getAll = budgetObjectStore.getAll();
+
+    getAll.onsuccess = function () {
+        if (getAll.result.length > 0) {
+            fetch('/api/transaction', {
+                method: 'POST',
+                body: JSON.stringify(getAll.result),
+                headers: {
+                    Accept: 'application/json, text/plain, */*',
+                    'Content-Type': 'application/json'
+                }
+            })
+                .then(response => response.json())
+                .then(serverResponse => {
+                    if (serverResponse.message) {
+                        throw new Error(serverResponse);
+                    }
+                    const transaction = db.transaction(['new_transaction'], 'readwrite');
+                    const budgetObjectStore = transaction.objectStore('new_transaction');
+                    budgetObjectStore.clear();
+
+                    alert('All saved transactions has been submitted!');
+                })
+                .catch(err => {
+                    console.log(err);
+                });
+        }
+    }
+}
+
+// listen for app to come back online
+window.addEventListener('online', uploadTransaction);
